7e07fc65be0f5c240d8f7c95c3a6f668c6e8248f
rova53 <roockio@gmail.com>
2026-03-02 14:45:24 +0000

    Revert "try"
    This reverts commit 767f07bfbd08e04b40974327261173d1210825de.

--- extensions/whatsapp/index.ts
+++ extensions/whatsapp/index.ts
@@ -2,7 +2,6 @@ import type { OpenClawPluginApi } from "openclaw/plugin-sdk";
 import { emptyPluginConfigSchema } from "openclaw/plugin-sdk";
 import { whatsappPlugin } from "./src/channel.js";
 import { setWhatsAppRuntime } from "./src/runtime.js";
-import { registerWhatsAppSubagentHooks } from "./src/subagent-hooks.js";
 
 const plugin = {
   id: "whatsapp",
@@ -12,7 +11,6 @@ const plugin = {
   register(api: OpenClawPluginApi) {
     setWhatsAppRuntime(api.runtime);
     api.registerChannel({ plugin: whatsappPlugin });
-    registerWhatsAppSubagentHooks(api);
   },
 };
 
